9824c7c5b6e69bb2f81a3053a7d107114b499e95
Julia Alberto <julia.elyssa.alberto@gmail.com>
2023-06-14 15:15:42 -0700

    Upload One Social

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
   
 import './Footer.css'
 
+import gmail from "./../components/Socials/Instagram.svg";
+
 const Footer = () =>{
   return (
   <div class = "FooterContainer">
@@ -10,7 +12,7 @@ const Footer = () =>{
         <h2>LETS CONNECT!</h2>
         <p>I am currently looking for work opportunites</p>
         <div class ="LinkContainer">
-            <div class="Link"></div>
+        <div class="Link"> <img src={gmail} alt="Image" /> </div>
             <div class="Link"></div>
             <div class="Link"></div>
             <div class="Link"></div>
