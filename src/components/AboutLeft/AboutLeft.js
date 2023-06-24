aed8095ab0c3cabaec00d87ae435493e2ef5f510
Julia Alberto <julia.elyssa.alberto@gmail.com>
2023-06-23 23:17:03 -0700

    Fixed text alignment

--- src/components/AboutLeft/AboutLeft.js
+++ src/components/AboutLeft/AboutLeft.js
@@ -6,10 +6,13 @@ import { Link } from "react-router-dom";
 
 const AboutLeft = () =>{
     return(
-             <div class="WorkLeftContainer">
-                <h1> Work </h1>
-                <p> As of <b>Summer 2022</b>, </p>
-                <p> I am a <b> Website Manager </b> for <a href ="https://aifs.ucdavis.edu/"><b>AIFS - AI Institute for Food Systems</b> </a></p>
+             <div class="AboutLeftContainer">
+                <div className='AboutLeftText'>
+                <h1> About </h1>
+                <pre> I am an <b>Cognitive Science</b> student with a minor in <b>Computer Science</b> at <b>UC Davis</b>! </pre>
+
+                <p>I aspire to be a <b> full-stack developer</b> designing accessible, interactive, and efficient web and mobile applications for all abilities and ages.</p>
+                </div>
                 <Link to="/resume"> <button class= "AboutButton"> <b>Download Resume </b></button></Link> 
                 
 
--- src/components/WorkLeft/WorkLeft.js
+++ src/components/WorkLeft/WorkLeft.js
@@ -7,9 +7,11 @@ import { Link } from "react-router-dom";
 const WorkLeft = () =>{
     return(
              <div class="WorkLeftContainer">
+                <div className='WorkLeftText'>
                 <h1> Work </h1>
-                <p> As of <b>Summer 2022</b>, </p>
-                <p> I am a <b> Website Manager </b> for <a href ="https://aifs.ucdavis.edu/"><b>AIFS - AI Institute for Food Systems</b> </a></p>
+                <pre> As of <div className='bold'>Summer 2022</div>,</pre>
+                <pre> I am a<b> Website Manager </b>for <a href ="https://aifs.ucdavis.edu/"> AIFS - AI Institute for Food Systems </a></pre>
+                </div>
                 <Link to="/resume"> <button class= "WorkButton"> <b>Download Resume </b></button></Link> 
                 
 
