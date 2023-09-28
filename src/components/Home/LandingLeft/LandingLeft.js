962a11c3047574aefe8113fbd70a622b8693237f
Julia Alberto <julia.elyssa.alberto@gmail.com>
2023-09-27 22:43:33 -0700

    Changed Font sizes for devices and test animation

--- src/components/Home/LandingLeft/LandingLeft.js
+++ src/components/Home/LandingLeft/LandingLeft.js
@@ -3,18 +3,23 @@ import './LandingLeft.css';
 import * as React from 'react';
 import { Link } from "react-router-dom";
 
+import { Fade } from 'react-reveal';
 
 const LandingLeft = () =>{
     return(
+        
              <div class="LandingLeftContainer">
+                <Fade bottom cascade>
                 <h1> Julia Here! </h1>
-                 <h1>Hello There! 👋 </h1>
+                 <h1>Hello There! </h1>
                 <h2> I am a <b>Front-End Developer</b> from the Bay Area</h2>
                 <h2> I create <b> meaningful and accessible </b>applications and experiences </h2>
+                </Fade>
                 <Link to="/work" class= "LandingButton">  <b>View my work here! </b></Link>
                 
-
+                
             </div>
+            
     );
 }
  
