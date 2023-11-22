5c852a7a505d1ad22c02afb9e846986d2324c927
Julia Alberto <julia.elyssa.alberto@gmail.com>
2023-11-22 12:47:11 -0800

    Added effects and adjusted font size

--- src/components/About/AboutLeft/AboutLeft.js
+++ src/components/About/AboutLeft/AboutLeft.js
@@ -2,15 +2,18 @@ import './AboutLeft.css';
 
 import * as React from 'react';
 
+import { Fade } from 'react-reveal';
 
 const AboutLeft = () =>{
     return(
              <div class="AboutLeftContainer">
                 <div className='AboutLeftText'>
+                <Fade bottom cascade>
                 <h1> About </h1>
                 <h2> I am an <b>Cognitive Science</b> student with a minor in <b>Computer Science</b> at <b>UC Davis</b> </h2>
 
                 <h2>I aspire to be a <b> full-stack developer</b> designing accessible, interactive, and efficient web and mobile applications for all abilities and ages</h2>
+                </Fade>
                 </div> 
                 
 
--- src/components/Work/WorkLeft/WorkLeft.js
+++ src/components/Work/WorkLeft/WorkLeft.js
@@ -3,14 +3,17 @@ import './WorkLeft.css';
 import * as React from 'react';
 import { Link } from "react-router-dom";
 
+import { Fade } from 'react-reveal';
 
 const WorkLeft = () =>{
     return(
              <div class="WorkLeftContainer">
                 <div className='WorkLeftText'>
+                <Fade bottom cascade>
                 <h1> Work </h1>
                 <h2> As of <b>Summer 2022,</b></h2>
                 <h2> I am a<b> Website Manager </b>for <a target="_blank" href ="https://aifs.ucdavis.edu/"> AIFS - AI Institute for Food Systems </a></h2>
+                </Fade>
                 </div>
                 <Link to="/resume"> <button class= "WorkButton"> <b>Download Resume </b></button></Link> 
                 
