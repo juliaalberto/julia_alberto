628346fcd7bff2eadd92183af13a619837ede884
Julia Alberto <julia.elyssa.alberto@gmail.com>
2023-12-20 16:48:13 -0800

    Fixed Accordion and cards spacing

--- src/components/Work/WorkAccordion/WorkAccordionItem.js
+++ src/components/Work/WorkAccordion/WorkAccordionItem.js
@@ -9,7 +9,7 @@ const WorkAccordionItem = ({ title, content }) => {
     return (
       <div className="accordion-item">
         <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-          <div>{title}</div>
+          <div className="accordiontitletext">{title}</div>
           <div>{isActive ? '-' : '+'}</div>
         </div>
         {isActive && <div className="accordion-content">{content}</div>}
