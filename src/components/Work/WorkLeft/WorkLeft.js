9426dd4a6c38d52fa18503fbc8ed9c15559a5159
Julia Alberto <julia.elyssa.alberto@gmail.com>
2024-01-25 14:24:59 -0800

    changed text on work page

--- src/components/Work/WorkLeft/WorkLeft.js
+++ src/components/Work/WorkLeft/WorkLeft.js
@@ -12,7 +12,7 @@ const WorkLeft = () =>{
                 <Fade bottom cascade>
                 <h1> Work </h1>
                 <h2> As of <b>Summer 2022,</b></h2>
-                <h2> I am a<b> Website Manager </b>for <a target="_blank" href ="https://aifs.ucdavis.edu/"> AIFS - AI Institute for Food Systems </a></h2>
+                <h2> I am a<b> Website Developer </b>for <a target="_blank" href ="https://aifs.ucdavis.edu/"> AIFS - AI Institute for Food Systems </a></h2>
                 </Fade>
                 </div>
                 <Link to="/resume"> <button class= "WorkButton"> <b>Download Resume </b></button></Link> 
