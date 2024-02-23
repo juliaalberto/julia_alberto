b4e8f96b65ee4a3940c3a8e50252bf469b5ca520
Julia Alberto <julia.elyssa.alberto@gmail.com>
2024-02-22 16:13:35 -0800

    Fixed spacing for home page

--- src/components/About/AboutLeft/AboutLeft.js
+++ src/components/About/AboutLeft/AboutLeft.js
@@ -10,14 +10,14 @@ const AboutLeft = () =>{
                 <div className='AboutLeftText'>
                 <Fade bottom cascade>
                 <h1> About </h1>
-                <h2> I am an student at <b>UC Davis</b> majoring in <b>Cognitive Science</b> with a <b>Computational Emphasis</b> and minoring in <b>Computer Science</b> and <b>Education</b></h2>
+                <h2> I am an undergradute student at <b>UC Davis</b> majoring in <b>Cognitive Science</b> with a <b>Computational Emphasis</b> and minoring in <b>Computer Science</b> and <b>Education</b></h2>
 
                 <h2>
                     I continuously orient myself to promote <b>social and educational</b> change, awareness, and acceptance through different <b>accessible and interactive</b> experiences and applications for all abilities and ages
                 </h2>    
 
                 <h2>
-                    I aspire to <b>educate children</b> in reaching their full potential and to be a freelance <b> full-stack developer</b></h2>
+                    I aspire to become a <b>youth educator</b> and a freelance <b>full-stack developer</b></h2>
                 </Fade>
                 </div> 
                 
